a0f4a66cf442aaf14cf0b622ff686bfbc7ca6eff
Sir Frankie Crisp <sirfrankiecrisp@pm.me>
2020-12-03 13:41:05 +0100

    Add test program to run main and client together

--- test.erl
+++ test.erl
@@ -1,15 +1,9 @@
 -module(test).
--export([test/0,test_code/0]).
+-export([start/0]).
+-import(main, [init/0]).
+-import(client, [client_init/0]).
 
 
-test() ->
-    register(pid, spawn(?MODULE, test_code, [])),
-    pid ! {hello},
-    ok.
-
-test_code() ->
-    receive
-        {hello} ->
-            io:format("~p~n",[self()])
-    end,
-    test_code().
+start() ->
+    main:init(),
+    client:client_init().
